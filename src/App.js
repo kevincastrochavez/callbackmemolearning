9ba312750755f37105e940e2b9e10498d017c3bc
Kevin Castro <kevincastro@C02FD2RV-0db637.local>
2023-02-08 13:08:39 -0700

    created input and button with some functionality to toggle color

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [number, setNumber] = useState(false);
+  const [dark, setDark] = useState(false);
+
+  const theme = {
+    backgroundColor: dark ? "#333" : "#fff",
+    color: dark ? "#fff" : "#333",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={theme}>
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(parseInt(e.target.value))}
+      />
+
+      <button onClick={() => setDark((prevDark) => !prevDark)}>
+        Toggle theme
+      </button>
     </div>
   );
 }
