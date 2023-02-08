5af8a0e7ef4de26157faee47e52695e19273b696
Kevin Castro <kevincastro@C02FD2RV-0db637.local>
2023-02-08 13:15:56 -0700

    created the list component, and imported into app. Added function that mocks data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import { useState } from "react";
 import "./App.css";
+import List from "./List";
 
 function App() {
-  const [number, setNumber] = useState(false);
+  const [number, setNumber] = useState(1);
   const [dark, setDark] = useState(false);
+
+  const getItems = () => {
+    return [number, number + 1, number + 2];
+  };
 
   const theme = {
     backgroundColor: dark ? "#333" : "#fff",
@@ -21,6 +26,8 @@ function App() {
       <button onClick={() => setDark((prevDark) => !prevDark)}>
         Toggle theme
       </button>
+
+      <List getItems={getItems} />
     </div>
   );
 }
